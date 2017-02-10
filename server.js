0c6b128279bfaf43ee524c573f99b9a10da2f526
harshit158 <harshit158@gmail.com>
2017-02-10 23:32:17 +0530

    initial

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var	app = express();
+var	bodyParser = require('body-parser');
+var morgan = require('morgan');
+const firebase= require('firebase');
+
+// app.use(morgan('dev'));
+
+// Initializing firebase 
+var config = {
+  apiKey: "AIzaSyCUcgbm-0Yfhv1YF1UREO7iy8zZdoSNl5s",
+  authDomain: " fetchfind-12fc9.firebaseapp.com",
+  databaseURL: "https://fetchfind-12fc9.firebaseio.com",
+};
+
+firebase.initializeApp(config);
+var db = firebase.database()
+
+db.ref('Users/User 4').set({Lost:'Harshit Sharma' ,
+						  Found:'Sindhra'})
+
+// BASE ROUTES 
+
+app.get('/', function(req, res) {
+	
+	res.send("Welcome to Fetchfind");
+});
+
+app.get('/get', function(req, res) {
+	
+	db.ref('/Users').on('value',function(snap){
+		res.send(snap.val());
+	})
+});
+
+// app.get('/Users/:userId', function(req, res) {
+// 	var userid=req.params.userId
+// 	db.ref('Users/'+userid).set({Lost:'Harshit Sharma' ,
+// 						  Found:'Sindhra'})
+// 	res.send(req.params);
+// });
+
+
+
+// start the server
+
+app.listen('3000', function(){
+	console.log('server is running')
+});
