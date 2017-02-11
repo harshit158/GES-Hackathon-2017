ba33c688d554d6670498621f11dba290d5d9cf53
Harshit <harshit158@gmail.com>
2017-02-11 15:18:19 +0530

    m

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ app.get('/length', function(req, res) {
 	// var address = addressForLength;
 	db.ref('/Users').once('value',function(snap){
 		var jsonObject = JSON.stringify(snap.val());
-		res.send(jsonObject);
+		var count=0
+		for (var user in jsonObject){
+			count++;
+		}
+		res.send(count);
 	});
 });
 
