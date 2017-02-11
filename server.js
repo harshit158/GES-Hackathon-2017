fabf15166bebe53892eea817c016cab9fdece735
Harshit <harshit158@gmail.com>
2017-02-11 11:32:31 +0530

    mod

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var	app = express();
+const firebase= require('firebase');
 
-app.set('port', (process.env.PORT || 5000))
-var morgan = require('morgan');
-// const firebase= require('firebase');
+app.set('port', (process.env.PORT || 3000))
 
-// app.use(morgan('dev'));
 
 // Initializing firebase 
 var config = {
@@ -14,36 +12,30 @@ var config = {
   databaseURL: "https://fetchfind-12fc9.firebaseio.com",
 };
 
-// firebase.initializeApp(config);
-// var db = firebase.database()
+firebase.initializeApp(config);
+var db = firebase.database()
 
-// db.ref('Users/User 4').set({Lost:'Harshit Sharma' ,
-						  // Found:'Sindhra'})
+// db.ref('Users/User 4').set({Lost:'Harshit Sharma',Found:'Sindhra'})
 
 // BASE ROUTES 
 
 app.get('/', function(req, res) {
-	
 	res.send("Welcome to Fetchfind");
 });
 
-// app.get('/get', function(req, res) {
-	
-// 	db.ref('/Users').once('value',function(snap){
-// 		res.send(snap.val());
-// 	})
-// });
+app.get('/getdata', function(req, res) {
+	db.ref('/Users').once('value',function(snap){
+		res.send(snap.val());
+	});
+});
 
-// app.get('/Users/:userId', function(req, res) {
-// 	var userid=req.params.userId
-// 	db.ref('Users/'+userid).set({Lost:'Harshit Sharma' ,
-// 						  Found:'Sindhra'})
-// 	res.send(req.params);
-// });
-
-
+app.get('/Users/:userId', function(req, res) {
+	var userid=req.params.userId;
+	db.ref('Users/'+userid).set({Lost:'Key' ,Found:'Wallet'});
+	res.send(req.params);
+});
 
 // start the server
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
 })
