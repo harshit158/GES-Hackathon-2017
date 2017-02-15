3a8bb3f60508b1dce7b5b163a029b9b90446ee2a
Harshit <harshit158@gmail.com>
2017-02-15 18:02:47 +0530

    m

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.set('port', (process.env.PORT || 3000))
 // Initializing firebase -----------------------
 
 var config = {
-  apiKey: "AIzaSyCUcgbm-0Yfhv1YF1UREO7iy8zZdoSNl5s",
-  authDomain: " fetchfind-12fc9.firebaseapp.com",
-  databaseURL: "https://fetchfind-12fc9.firebaseio.com",
+	apiKey: "AIzaSyCUcgbm-0Yfhv1YF1UREO7iy8zZdoSNl5s",
+	authDomain: " fetchfind-12fc9.firebaseapp.com",
+	databaseURL: "https://fetchfind-12fc9.firebaseio.com",
 };
 
 firebase.database.enableLogging(true);
@@ -21,7 +21,7 @@ var db = firebase.database()
 // Routes ---------------------------------------
 // Checking the server
 app.get('/', function(req, res) {
-	 res.send("Welcome to Fetchfind");
+	res.send("Welcome to Fetchfind");
 	
 });
 
@@ -57,59 +57,61 @@ app.get('/sendprocessretrieve/:Itemdata', function(req, res) {
 		var countryCode=JSON.parse(result)["countryCode"];
 
 		if(islost===true){
-		//LOST 	
-		//Update lost items , Match items , Send status to bot
-		db.ref(countryCode+'/'+ data["itemtype"]+'/lost').push(data);
-		initiatematchingwithfound(countryCode, data); //this function runs each time refresh is called
+				//LOST 	
+				//Update lost items , Match items , Send status to bot
+				db.ref(countryCode+'/'+ data["itemtype"]+'/lost').push(data);
+				initiatematchingwithfound(countryCode, data); //this function runs each time refresh is called
 
-		}
-		else{
-		//FOUND
-		//Update found items , Match items , Send status to bot
-		db.ref(countryCode+'/'+ data["itemtype"]+'/found').push(data);
+			}
+			else{
+				//FOUND
+				//Update found items , Match items , Send status to bot
+				db.ref(countryCode+'/'+ data["itemtype"]+'/found').push(data);
+			}
 
-		}
+		});	
 
-		});
+});
 
-
-	var initiatematchingwithfound=function(countryCode, lostdata){
+function initiatematchingwithfound(countryCode, lostdata){
 	db.ref(countryCode+'/'+data["itemtype"]+'/found').once('value',function(snap){
 		var founditemsincountry = Object.values(snap.val());
- 
-          var items=[];
 
-          for(var i=0;i<founditemsincountry.length;i++){
-          	var iteratedfounditem=founditemsincountry[i];
-          	var distance=calcCrow(iteratedfounditem,lostdata);
-          	if(distance<5 && priceCheck(iteratedfounditem.reward,lostdata.reward)===true){
+		var items=[];
+
+		for(var i=0;i<founditemsincountry.length;i++){
+			var iteratedfounditem=founditemsincountry[i];
+			var distance=calcCrow(iteratedfounditem,lostdata);
+			if(distance<5 && priceCheck(iteratedfounditem.reward,lostdata.reward)===true){
           			//prepare this item now
-          		var prepareditem={
-          			"title":"Match "+i+1,
-          			"subtitle":distance+" km away | Reward demanded: "+iteratedfounditem.reward,
-          			"imgurl":iteratedfounditem.foundimage,
-          			"options":[
-          			{
-          				"type":"text",
-          				"title":iteratedfounditem.userid+'#'+i
-          			}]
-          		};
-          		items.push(prepareditem);
-          	}	
-          }
+          			var prepareditem={
+          				"title":"Match "+i+1,
+          				"subtitle":distance+" km away | Reward demanded: "+iteratedfounditem.reward,
+          				"imgurl":iteratedfounditem.foundimage,
+          				"options":[
+          				{
+          					"type":"text",
+          					"title":iteratedfounditem.userid+'#'+i
+          				}]
+          			};
+          			items.push(prepareditem);
+          		}	
+          	}
           //all weapons deployed
           //ab goli maaro
           res.send(items);
       }
-	
-	function priceCheck(foundPrice,lostPrice){
-		var fp=foundPrice.split(' ')[1];
-		var lp=lostPrice.split(' ')[1];
-		return fp<=2*lp;
-	}
-	function calcCrow(data1,data2) 
-    {
-    	var lat1=Number(data1["lat"]);var lon1=Number(data1["lang"]);var lat2=Number(data2["lat"]);var lon2=Number(data2["lang"]);
+  }
+
+      function priceCheck(foundPrice,lostPrice){
+      	var fp=foundPrice.split(' ')[1];
+      	var lp=lostPrice.split(' ')[1];
+      	return fp<=2*lp;
+      }
+
+
+      function calcCrow(data1,data2){
+      	var lat1=Number(data1["lat"]);var lon1=Number(data1["lang"]);var lat2=Number(data2["lat"]);var lon2=Number(data2["lang"]);
     	//lat1, lon1, lat2, lon2
 	    var R = 6371; // km
 	    var dLat = toRad(lat2-lat1);
@@ -118,12 +120,11 @@ app.get('/sendprocessretrieve/:Itemdata', function(req, res) {
 	    var lat2 = toRad(lat2);
 
 	    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-	      Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+	    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
 	    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 	    var d = R * c;
 	    return d.toFixed(2);
 	}
-}
 
 // Searching if a user has already submitted any preivious request
 app.get('/Users/search/:userId', function(req, res) {
@@ -152,5 +153,5 @@ app.get('/search/:userId', function(req, res) {
 
 // start the server
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+	console.log("Node app is running at localhost:" + app.get('port'));
 })
